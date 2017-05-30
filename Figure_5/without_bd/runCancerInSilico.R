4caec9f8a894a971c803423bbe1bf239c84953d4
rcao5 <rcao5@jhu.edu>
2017-05-30 14:43:58 -0400

    data for no boundary

---
+++ Figure_5/without_bd/runCancerInSilico.R
@@ -0,0 +1,83 @@
+library('CancerInSilico')
+library(methods)
+
+args <- commandArgs(TRUE)
+arrayNum <- as.integer(args[1])
+returnSize <- as.integer(args[2])
+
+#### Set Defaults ####
+
+initialNum <- 80
+runTime <- 168
+density <- 0.2
+boundary <- 1
+syncCycles <- FALSE
+randSeed <- 0
+outputIncrement <- 4
+recordIncrement <- 0.25
+timeIncrement <- 0.001
+cellTypes <- c(new('CellType', name='DEFAULT'))
+cellTypeInitFreq <- c(1)
+drugs <- list()
+maxDeformation <- 0.1
+maxTranslation <- 0.1
+maxRotation <- 0.3
+nG <- 28
+epsilon <- 10.0
+delta <- 0.2
+
+#### Set Custom Values ####
+
+allInitFreq_typeA <- seq(0,1,0.05)
+allCellTypes_typeB <- lapply(seq(12,36,4), function(l) new('CellType', name='B', minCycle=l, cycleLength=function() l))
+
+dim <- c(length(allInitFreq_typeA), length(allCellTypes_typeB))
+indexArray <- array(1:prod(dim), dim)
+index <- which(indexArray==arrayNum, arr.ind=TRUE)
+
+initFreq_typeA <- allInitFreq_typeA[index[1]]
+ctB <- allCellTypes_typeB[index[2]]
+ctA <- new('CellType', name='A', cycleLength=function() {return(36)}, minCycle=36)
+
+cellTypes <- c(ctA, ctB)
+cellTypeInitFreq <- c(initFreq_typeA, 1 - initFreq_typeA)
+boundary <- 0
+
+#### Run Simulation ####
+
+if (!is.na(returnSize)) {
+
+    cat(as.numeric(prod(dim)))
+
+} else {
+
+    repeat
+    {
+        output <- runCellSimulation(initialNum=initialNum,
+            runTime=runTime,
+            density=density,
+            boundary=boundary,
+            syncCycles=syncCycles,
+            randSeed=randSeed,
+            modelType=modelType,
+            outputIncrement=outputIncrement,
+            recordIncrement=recordIncrement,
+            timeIncrement=timeIncrement,
+            cellTypes=cellTypes,
+            cellTypeInitFreq=cellTypeInitFreq,
+            drugs=drugs,
+            maxDeformation=maxDeformation,
+            maxTranslation=maxTranslation,
+            maxRotation=maxRotation,
+            nG=nG,
+            epsilon=epsilon,
+            delta=delta
+        )
+        randSeed <- randSeed + 100
+
+        if (length(output@cells) > output@runTime / output@recordIncrement)
+            break
+    }
+
+    save(output, file=paste("output_", arrayNum, ".RData", sep=""))
+}
