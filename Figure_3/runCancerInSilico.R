d57a39e3a57577a86b94540897f11d5f43aaf1a1
tsherma4 <tsherma4@jhu.edu>
2017-05-30 12:17:14 -0400

    Figure 3

---
+++ Figure_3/runCancerInSilico.R
@@ -0,0 +1,85 @@
+library('CancerInSilico')
+library(methods)
+
+args <- commandArgs(TRUE)
+arrayNum <- as.integer(args[1])
+returnSize <- as.integer(args[2])
+
+#### Set Defaults ####
+
+initialNum <- 80
+runTime <- 168
+density <- 0.2
+boundary <- 1
+syncCycles <- FALSE
+randSeed <- 0
+outputIncrement <- 4
+recordIncrement <- 0.25
+timeIncrement <- 0.001
+cellTypes <- c(new('CellType', name='DEFAULT'))
+cellTypeInitFreq <- c(1)
+drugs <- list()
+maxDeformation <- 0.1
+maxTranslation <- 0.1
+maxRotation <- 0.3
+nG <- 28
+epsilon <- 10.0
+delta <- 0.2
+
+#### Set Custom Values ####
+
+allDensities <- seq(0.05, 0.45, 0.05)
+allSyncs <- c(TRUE, FALSE)
+allCellTypes <- lapply(seq(12,48,4), function(l) new('CellType',
+    name='DEFAULT', minCycle=l, cycleLength=function() l))
+allDrugs <- lapply(seq(0.5, 1.0, 0.05), function(l) new('Drug',
+    name='DEFAULT', timeAdded=24, cycleLengthEffect=function(a,b) b*l))
+
+dim <- c(length(allDensities), length(allSyncs), length(allCellTypes),
+    length(allDrugs))
+indexArray <- array(1:prod(dim), dim)
+index <- which(indexArray==arrayNum, arr.ind=TRUE)
+
+density <- allDensities[index[1]]
+syncCycles <- allSyncs[index[2]]
+cellTypes <- c(allCellTypes[index[3]])
+drugs <- c(allDrugs[index[4]])
+
+#### Run Simulation ####
+
+if (!is.na(returnSize)) {
+
+    cat(as.numeric(prod(dim)))
+
+} else {
+
+    repeat
+    {
+        output <- runCellSimulation(initialNum=initialNum,
+            runTime=runTime,
+            density=density,
+            boundary=boundary,
+            syncCycles=syncCycles,
+            randSeed=randSeed,
+            modelType=modelType,
+            outputIncrement=outputIncrement,
+            recordIncrement=recordIncrement,
+            timeIncrement=timeIncrement,
+            cellTypes=cellTypes,
+            cellTypeInitFreq=cellTypeInitFreq,
+            drugs=drugs,
+            maxDeformation=maxDeformation,
+            maxTranslation=maxTranslation,
+            maxRotation=maxRotation,
+            nG=nG,
+            epsilon=epsilon,
+            delta=delta
+        )
+        randSeed <- randSeed + 100
+
+        if (length(output@cells) > output@runTime / output@recordIncrement)
+            break
+    }
+
+    save(output, file=paste("output_", arrayNum, ".RData", sep=""))
+}
