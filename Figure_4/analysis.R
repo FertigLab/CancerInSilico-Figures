ad86a7425b015760203a6d993841bfe1a200cdb2
sherman5 <tomsherman159@gmail.com>
2017-06-06 15:00:08 -0400

    fig 4 rough draft

--- Figure_4/analysis.R
+++ Figure_4/analysis.R
@@ -17,12 +17,13 @@ getMeanScale <- function(mod, pwy)
 
 ## Figure 4b - pathway activity for fitted run
 
-pwyActivity <- data.frame(
+pwyActivity <- data.frame(hour=0:168,
     activationGrowth=getMeanScale(fig4Data[[1]], pwyGrowth),
     GtoS=getMeanScale(fig4Data[[1]], pwySPhase),
     mitosis=getMeanScale(fig4Data[[1]], pwyMitosis),
-    contactInhibition=getMeanScale(fig4Data[[1]], pwyContactInhibition))
+    contactInhibition=1 - getMeanScale(fig4Data[[1]], pwyContactInhibition))
 
-
-
-
+fig <- ggplot(pwyActivity, aes(x=hour)) + geom_line(aes(y=GtoS)) +
+    geom_line(aes(y=activationGrowth)) + geom_line(aes(y=mitosis)) + 
+    geom_line(aes(y=contactInhibition))
+ggsave(filename='fig4b.png', plot=fig)
