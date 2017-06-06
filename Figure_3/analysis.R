577d4d0a09894a4a1993f6552b12f8997154678d
Tom Sherman <tsherma4@jhu.edu>
2017-06-06 08:45:29 -0400

    removed README added data

--- Figure_3/analysis.R
+++ Figure_3/analysis.R
@@ -1,7 +1,8 @@
-library(ggplot2)
+#library(ggplot2)
 library(methods)
 library(CancerInSilico)
 load('Figure_3_cleaned.RData') #fig3Data
+load('tangData.RData') #tangData
 
 len <- length(fig2Data[[1]]$numCells)
 rawData <- lapply(fig2Data, function(mod)
@@ -17,8 +18,20 @@ data <- data.frame(time=rawData[,1], numCells=rawData[,2],
 
 ## Figure 3a - fit real data w/out sync
 
-fig <- ggplot(subset(data, initDen==0.05 & cycleLength==24), 
-    aes(x=time, y=numCells, col=drugEffect)) + geom_point()
-ggsave(filename='fig3a.png', plot=fig)
+#fig <- ggplot(subset(data, initDen==0.05 & cycleLength==24), 
+#   aes(x=time, y=numCells, col=drugEffect)) + geom_point()
+#ggsave(filename='fig3a.png', plot=fig)
 
-## Figure 3b - fit real data w/ sync
+#fig <- ggplot(tangData, aes(x=day, y=numCells, col=dosage)) + geom_point()
+#ggsave(filename='rawData.png')
+
+png(filename='fig3a.png')
+
+plot.new()
+plot.window(xlim=c(0,max(tangData$day)), ylim=c(min(tangData$numCells),
+    max(tangData$numCells)))
+axis(side=1)
+axis(side=2)
+invisible(sapply(noBound, function(i) lines(fig2Data[[i]]$numCells)))
+invisible(sapply(bound, function(i) lines(fig2Data[[i]]$numCells)))
+invisible(dev.off())
