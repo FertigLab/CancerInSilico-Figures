1fe0560bbdfdf5afb592c8b3e34be4eb6ffe61c0
tsherma4 <tsherma4@jhu.edu>
2017-07-06 12:20:25 -0400

    fig 4 data

--- Figure_4/cleanData.R
+++ Figure_4/cleanData.R
@@ -4,19 +4,39 @@ library(CancerInSilico)
 allFiles <- list.files(path='../Data/Figure_4', full.names = TRUE,
     recursive = TRUE, pattern = "*.RData")
 
-fileNo <- 1
-pb <- txtProgressBar(min=1, max=length(allFiles), style=3)
 fig4Data <- list()
 for (file in allFiles)
 {
     load(file)
     fig4Data[[file]] <- output
-    
-    fileNo <- fileNo + 1
-    setTxtProgressBar(pb, fileNo)
 }
-close(pb)
-print('saving...')
-save(fig4Data, file='Figure_4_cleaned.RData')
 
+data(SamplePathways)
+
+sampFreq <- 4
+nCells <- 50
+hours <- seq(0,144,sampFreq)
+col_ndx <- 1:length(hours)
+
+pwyGrowth <- calibratePathway(pwyGrowth, referenceGeneExpression)
+pwyMitosis <- calibratePathway(pwyMitosis, referenceGeneExpression)
+pwySPhase <- calibratePathway(pwySPhase, referenceGeneExpression)
+pwyContactInhibition <- calibratePathway(pwyContactInhibition,
+    referenceGeneExpression)
+allPwys <- c(pwyGrowth, pwyMitosis, pwySPhase, pwyContactInhibition)
+
+ge_pbs <- inSilicoGeneExpression(fig4Data[[1]], allPwys, nCells=nCells,
+    sampFreq=sampFreq, perError=0.05)
+
+ge_10ug <- inSilicoGeneExpression(fig4Data[[2]], allPwys, nCells=nCells,
+    sampFreq=sampFreq, perError=0.05)
+
+ge_100ug <- inSilicoGeneExpression(fig4Data[[3]], allPwys, nCells=nCells,
+    sampFreq=sampFreq, perError=0.05)
+
+ge_pbs$expression <- ge_pbs$expression[,col_ndx]
+ge_10ug$expression <- ge_10ug$expression[,col_ndx]
+ge_100ug$expression <- ge_100ug$expression[,col_ndx]
+
+save(ge_pbs, ge_10ug, ge_100ug, file='Figure_4_cleaned.RData')
 
