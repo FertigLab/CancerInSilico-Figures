a5b2816d7d75449c4a136694b08ad4b57b722d86
sherman5 <tomsherman159@gmail.com>
2017-07-26 14:28:21 -0400

    fig 5 analysis

--- Figure_5/analysis.R
+++ Figure_5/analysis.R
@@ -5,23 +5,27 @@ library('reshape2')
 library(methods)
 load("Figure_5_cleaned.RData")
 
-mat <- matrix(nrow=length(cellTypeBInitFreq), ncol=length(cellTypeBCycleLength))
-for (data in fig5data)
-{
-	xind <- which(cellTypeBInitFreq == data[1])
-	yind <- which(cellTypeBCycleLength == data[2])
-	mat[xind, yind] <- data[3]
-}
-
-fpm <- mat
-fpm.melted <- melt(fpm)
-hm.palette <- colorRampPalette(rev(brewer.pal(9, 'YlOrRd')), space='Lab')
-fig <- ggplot(fpm.melted, aes(x = Var1, y = Var2, fill = value)) +
-        geom_tile() +
-        scale_fill_gradientn(colours = hm.palette(100)) +
-        ylab('Cycle Length of Faster Growing Cell Type') +
-        xlab('Initial Proportion of Slower Growing Cell Type') +
-        ggtitle("With boundary") +
-        theme(plot.title = element_text(hjust = 0.5)) +
-        theme(axis.text=element_text(size=10))
+fig <- ggplot(fig5data, aes(x=time)) + 
+    geom_point(aes(y=cellTypeBFreq)) 
 ggsave(filename='fig5a.png', plot=fig)
+
+#mat <- matrix(nrow=length(cellTypeBInitFreq), ncol=length(cellTypeBCycleLength))
+#for (data in fig5data)
+#{
+#	xind <- which(cellTypeBInitFreq == data[1])
+#	yind <- which(cellTypeBCycleLength == data[2])
+#	mat[xind, yind] <- data[3]
+#}
+#
+#fpm <- mat
+#fpm.melted <- melt(fpm)
+#hm.palette <- colorRampPalette(rev(brewer.pal(9, 'YlOrRd')), space='Lab')
+#fig <- ggplot(fpm.melted, aes(x = Var1, y = Var2, fill = value)) +
+#        geom_tile() +
+#        scale_fill_gradientn(colours = hm.palette(100)) +
+#        ylab('Cycle Length of Faster Growing Cell Type') +
+#        xlab('Initial Proportion of Slower Growing Cell Type') +
+#        ggtitle("With boundary") +
+#        theme(plot.title = element_text(hjust = 0.5)) +
+#        theme(axis.text=element_text(size=10))
+
