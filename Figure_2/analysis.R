d81c55f046c02f730327fd3820f7dd9f233aa0c7
sherman5 <tomsherman159@gmail.com>
2018-02-22 13:06:39 -0500

    updated all figs besides 5

--- Figure_2/analysis.R
+++ Figure_2/analysis.R
@@ -9,7 +9,7 @@ rawData <- lapply(fig2Data, function(mod)
     {
         N <- length(mod$numCells)
         timePoints <- 1:N - 1
-        if (mod$initDensity == 0.10 & (mod$cycleLength %in% c(12,20,28,36,44)))
+        if (mod$initDensity == 0.10 & (mod$cycleLength %in% c(12,28,44)))
         {
             cbind(timePoints, mod$numCells, rep(mod$cycleLength, N),
                 rep(ifelse(mod$boundary > 0, TRUE, FALSE), N))
@@ -25,8 +25,8 @@ fig <- ggplot(df, aes(x=time, y=nCells, linetype=bd,
     scale_linetype_manual(values=c("dashed", "solid")) +
     scale_y_continuous(trans='log10', breaks=c(100,200,500,1000)) +
     labs(title = "Sensitivity to Growth Rate and Boundary Presence", 
-        caption = "Figure 2a", x = "Time", y = "Number Of Cells (log scale)",
-        linetype = "Boundary") 
+        caption = "Figure 2a, expected cycle lengths are 12,28,44 (hrs) from left to right",
+        x = "Time", y = "Number Of Cells (log scale)", linetype = "Boundary") 
 
 ggsave(filename='fig2a.pdf', plot=fig)
 
@@ -49,7 +49,7 @@ rawData <- lapply(fig2Data, function(mod)
     {
         N <- length(mod$density)
         timePoints <- 1:N - 1
-        if (mod$cycleLength == 24 & (mod$initDensity %in% c(0.05,0.1,0.2,0.3,0.4)))
+        if (mod$cycleLength == 24 & (mod$initDensity %in% c(0.05,0.2,0.4)))
         {
             cbind(timePoints, movAvg(mod$density, 4), rep(mod$initDensity, N),
                 rep(ifelse(mod$boundary > 0, TRUE, FALSE), N))
@@ -64,7 +64,7 @@ fig <- ggplot(df, aes(x=time, y=den, linetype=bd,
     group=interaction(iDen, bd))) + geom_line() +
     scale_linetype_manual(values=c("dashed", "solid")) +
     labs(title = "Boundary Presence Effects Maximum Population Density", 
-        caption = "Figure 2b", x = "Time", y = "Population Density",
-        linetype = "Boundary")
+        caption = "Figure 2b, varying the initial density and holding growth rates constant",
+        x = "Time", y = "Population Density", linetype = "Boundary")
 
 ggsave(filename='fig2b.pdf', plot=fig)
